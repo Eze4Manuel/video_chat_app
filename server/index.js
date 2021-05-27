26cf2000559342d0ef2c8ba48ef9655be1767cc7
Ezenagu <emmanuelezenagu5@gmail.com>
2021-05-27 20:02:35 +0100

    On test branch trial

--- server/index.js
+++ server/index.js
@@ -1,52 +1,45 @@
+
 const express = require("express");
 const next = require("next");
 const http = require("http");
-const socketIO  = require("socket.io");
+const socketio  = require("socket.io");
 
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
-
-
-const PORT = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== "production";
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
-nextApp.prepare().then(() => {
+nextApp.prepare().then(async() => {
+    const app = express();
+    const server = http.createServer(app);
+    const io = new socketio.Server();
+    io.attach(server);
+
+    app.get('/hello', async (_, res) => {
+        res.send('Hello World')
+    });
 
     io.on("connection", (socket) => {
       
-      socket.emit("me", socket.id)
+            socket.emit("me", socket.id)
+      
+            socket.on("disconnect", () => {
+              socket.broadcast.emit("callEnded")
+            })
+      
+            socket.on("callUser", (data) => {
+              io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+            })
+      
+            socket.on("answerCall", (data) => {
+              io.to(data.to).emit("callAccepted", data.signal)
+            })
 
-      socket.on("disconnect", () => {
-        socket.broadcast.emit("callEnded")
-      })
+      });
 
-      socket.on("callUser", (data) => {
-        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-      })
+    app.all('*', (req, res) => nextHandler(req, res));
 
-      socket.on("answerCall", (data) => {
-        io.to(data.to).emit("callAccepted", data.signal)
-      })
-    })
-
-
-    // Using Express Route to handle routing on the backend
-    const showRoutes = require("./routes/index.js");
-
-    app.use("/api", showRoutes(app));
-
-    app.get("*", (req, res) => {
-      return nextHandler(req, res);
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
     });
-    server.listen(PORT);
-
-  })
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
-
-  
+});
